d23755899f3b04d456a35f597dd23d53a5ec7b51
RobinPreble <robinpreble@LAPTOP-TVOTQ7HI.localdomain>
2022-07-21 14:29:54 -0700

    added proper response but its still just printing a fuck ton of numbers so.... progress???

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    io::{self, Read},
+    io::{self, Write, Read},
     net::TcpStream,
 };
 fn main() {
@@ -12,20 +12,19 @@ fn main() {
 }
 
 fn main_inner() -> io::Result<()> {
-    let mut stream = TcpStream::connect("www.yahoo.com")?; 
+    let mut stream = TcpStream::connect("www.apple.com:80")?; 
     println!("we made it");
-    //let response = "HTTP/1.1 200 OK\r\n\r\n"; 
+    let response = stream.write(b"HTTP/1.1 200 OK\r\n\r\n"); 
     let mut recieved: Vec<u8> = vec![]; 
     let mut buffer = [0u8; 256];
 
     loop {
         let bytes_read = stream.read(&mut buffer)?; 
         recieved.extend(buffer); 
-
-        if bytes_read == 0 {
+        if bytes_read < 256 {
             break
         }
     }
-    println!("{:?}", recieved);
+    println!("woo{:?}", recieved);
     Ok(())
 }
