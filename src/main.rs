892b20f235bce0719f86cff5ee68cf178280f1b3
RobinPreble <robinpreble@LAPTOP-TVOTQ7HI.localdomain>
2022-07-21 13:46:44 -0700

    main_inner is called now but it returns invalid socket address error

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ fn main() {
 
 fn main_inner() -> io::Result<()> {
     let mut stream = TcpStream::connect("www.yahoo.com")?; 
+    println!("we made it");
     //let response = "HTTP/1.1 200 OK\r\n\r\n"; 
     let mut recieved: Vec<u8> = vec![]; 
     let mut buffer = [0u8; 256];
