e716c7d897ba22778de65c03df1312833645f639
RobinPreble <robinpreble@LAPTOP-TVOTQ7HI.localdomain>
2022-07-21 13:38:54 -0700

    first attempt at doing it without looking at tim's

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,19 @@ fn main() {
 }
 
 fn main_inner() -> io::Result<()> {
-    
+    let mut stream = TcpStream::connect("www.yahoo.com")?; 
+    //let response = "HTTP/1.1 200 OK\r\n\r\n"; 
+    let mut recieved: Vec<u8> = vec![]; 
+    let mut buffer = [0u8; 256];
+
+    loop {
+        let bytes_read = stream.read(&mut buffer)?; 
+        recieved.extend(buffer); 
+
+        if bytes_read == 0 {
+            break
+        }
+    }
+    println!("{:?}", recieved);
     Ok(())
 }
