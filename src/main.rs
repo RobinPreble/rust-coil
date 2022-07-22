2a7e3c8c911fcaa74e7f5fb51fce212572e07acb
RobinPreble <robinpreble@LAPTOP-TVOTQ7HI.localdomain>
2022-07-22 15:57:53 -0700

    edited header

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     io::{self, Write, Read},
-    net::TcpStream,
+    net::{TcpStream, Shutdown},
     env
 };
 fn main() {
@@ -17,9 +17,10 @@ fn main() {
     }
 }
 
-fn main_inner(add: &str, port: &str) -> io::Result<()> {
-    let mut stream = TcpStream::connect(add.to_string() + ":" + port)?; 
-    let _response = stream.write(b"GET / HTTP/1.1\r\n\r\n"); 
+fn main_inner(host: &str, port: &str) -> io::Result<()> {
+    let mut stream = TcpStream::connect(host.to_string() + ":" + port)?; 
+    let header = format!("GET / HTTP/1.1\nUser-Agent: curl/7.77.0\nAccept: */*\nHost: {}:{}\nConnection: close\n\n", host, port);
+    let _result = stream.write(&header.as_bytes()).unwrap(); 
     let mut recieved: Vec<u8> = vec![]; 
     let mut buffer = [0u8; 256];
 
@@ -31,6 +32,7 @@ fn main_inner(add: &str, port: &str) -> io::Result<()> {
             break
         }
     }
+    //stream.shutdown(Shutdown::Both).expect("u done fucked up");
 
     let result = String::from_utf8(recieved).unwrap();
     println!("{}", result);
