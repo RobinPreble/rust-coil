50211469d7de8fa071728208b25b189a62ecd6fe
RobinPreble <robinpreble@LAPTOP-TVOTQ7HI.localdomain>
2022-07-25 14:27:31 -0700

    running with reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
-use std::{
-    io::{self, Write, Read},
-    net::{TcpStream, Shutdown},
-    env
-};
+use std::{env, io};
+
+
+//use openssl::ssl::{SslMethod, SslConnectorBuilder, SslConnector};
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
+    if args.len() != 2 {
         panic!("Please provide the address and port as arguments");
     }
     
-    match main_inner(&args[1], &args[2]) {
+    match main_inner(&args[1]) {
         Ok(_) => {},
         Err(e) => {
             println!("{}", e);
@@ -17,24 +16,8 @@ fn main() {
     }
 }
 
-fn main_inner(host: &str, port: &str) -> io::Result<()> {
-    let mut stream = TcpStream::connect(host.to_string() + ":" + port)?; 
-    let header = format!("GET / HTTP/1.1\nUser-Agent: curl/7.77.0\nAccept: */*\nHost: {}:{}\nConnection: close\n\n", host, port);
-    let _result = stream.write(&header.as_bytes()).unwrap(); 
-    let mut recieved: Vec<u8> = vec![]; 
-    let mut buffer = [0u8; 256];
-
-    loop {
-        let bytes_read = stream.read(&mut buffer)?; 
-        recieved.extend(buffer); 
-        buffer = [0u8; 256]; // reset buffer
-        if bytes_read == 0 {
-            break
-        }
-    }
-    //stream.shutdown(Shutdown::Both).expect("u done fucked up");
-
-    let result = String::from_utf8(recieved).unwrap();
-    println!("{}", result);
+fn main_inner(url: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let response = reqwest::blocking::get(url)?.text()?;
+    println!("{}", response);
     Ok(())
 }
