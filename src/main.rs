fb1a048cd457ad72efdeb4ebd0b3a9294d51410a
RobinPreble <robinpreble@LAPTOP-TVOTQ7HI.localdomain>
2022-07-25 19:31:28 -0700

    renamed main_inner and made it return the html rather than printing it

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,51 @@
-use std::{env, io};
+use std::env;
 use scraper::{Html, Selector};
 
-//use openssl::ssl::{SslMethod, SslConnectorBuilder, SslConnector};
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         panic!("Please provide the address and port as arguments");
     }
     
-    match main_inner(&args[1]) {
-        Ok(_) => {},
+    match get_raw_html(&args[1]) {
+        Ok(raw_html) => {
+            println!("{}", raw_html);
+        },
         Err(e) => {
             println!("{}", e);
         }
     }
 }
 
-fn main_inner(url: &str) -> Result<(), Box<dyn std::error::Error>> {
+fn get_raw_html(url: &str) -> Result<String, Box<dyn std::error::Error>> {
     let response = reqwest::blocking::get(url)?.text()?;
-    println!("{}", response);
+    //println!("{}", response);
     //response.matches(is_link());
 
-    let document = Html::parse_document(&response);
-    let link_selector = Selector::parse("a").unwrap();
+    // let document = Html::parse_document(&response);
+    // let link_selector = Selector::parse("a").unwrap();
     
 
-    let mut links: Vec<&str> = Vec::new();
+    // let mut links: Vec<&str> = Vec::new();
 
-    for lnk in document.select(&link_selector) {
-        let href = lnk.value().attr("href").unwrap_or("invalid link");
-        if href.starts_with("http"){
-            links.push(&href);
-        }
-    }
-    println!("Links: {:?}", links);
+    // for lnk in document.select(&link_selector) {
+    //     let href = lnk.value().attr("href").unwrap_or("invalid link");
+    //     if href.starts_with("http"){
+    //         links.push(&href);
+    //     }
+    // }
+    // println!("Links: {:?}", links);
 
-    let image_selector = Selector::parse("img").unwrap();
-    let mut images: Vec<&str> = Vec::new(); 
-    for thingy in document.select(&image_selector) {
-        let img = thingy.value().attr("src").unwrap_or("invalid image");
-        images.push(&img);
-    }
-    println!("Images: {:?}", images);
+    // let image_selector = Selector::parse("img").unwrap();
+    // let mut images: Vec<&str> = Vec::new(); 
+    // for thingy in document.select(&image_selector) {
+    //     let img = thingy.value().attr("src").unwrap_or("invalid image");
+    //     images.push(&img);
+    // }
+    // println!("Images: {:?}", images);
 
 
-    Ok(())
+    Ok(response)
 }
 
 
